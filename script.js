2b13feef0b0b6121ef8e53290a681e1c5599fe32
Will <asampongg377@gmail.com>
2024-09-14 10:54:16 +0000

    Init

--- script.js
+++ script.js
@@ -6,23 +6,44 @@ const tipPerPerson = document.querySelector('.tip-per-person-amount');
 const totalAmount = document.querySelector('.total-amount');
 const resetBtn = document.querySelector('.reset-button button');
 
+const updateDisplay = (tipAmount, totalAmountValue) => {
+    tipPerPerson.textContent = `$${tipAmount.toFixed(2)}`;
+    totalAmount.textContent = `$${totalAmountValue.toFixed(2)}`;
+}
 
 const tipCalculation = () => {
     tips.forEach((tip) => {
         tip.addEventListener('click', () => {
             console.log(tip.value);
-            const tipPercentage = tip.value;        
-        })
+            const tipPercentage = parseFloat(tip.value) / 100;
+            const tipAmount = tipOnBill(tipPercentage);
+            const totalAmountValue = totalAmountPerPerson(tipPercentage);
+            updateDisplay(tipAmount, totalAmountValue);
+
+        });
     });
-    
+
+    const tipOnBill = (tipPercentage) => {
+        let bill = parseFloat(billInput.value);
+        let people = parseFloat(numOfPeople.value) || 1;
+        return (tipPercentage * bill) / people;
+    }
+
+    const totalAmountPerPerson = (tipPercentage) => {
+        let bill = parseFloat(billInput.value);
+        let people = parseFloat(numOfPeople.value) || 1;
+        return (bill + (tipPercentage * bill)) /people
+    }
 }
 
+tipCalculation();
 
 
 
 resetBtn.addEventListener('click', () => {
     billInput.value = '';
     numOfPeople.value = '';
+    // customTip.value = '';
     tipPerPerson.textContent = '0.00';
     totalAmount.textContent = '0.00';
 })
